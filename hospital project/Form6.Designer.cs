5d24e779a1bc241699c8cb59e17add2b35d48d7c
Jubayer Hasan Akash <153942105+JubayerAkash@users.noreply.github.com>
2026-01-24 20:24:29 +0600

    user logo added

--- hospital project/Form6.Designer.cs
+++ hospital project/Form6.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonBack = new System.Windows.Forms.Button();
             this.ButtonSign = new System.Windows.Forms.Button();
@@ -49,10 +50,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::hospital_project.Properties.Resources.Copilot_20260122_0054041;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(1, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(475, 553);
+            this.pictureBox1.Size = new System.Drawing.Size(465, 541);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
